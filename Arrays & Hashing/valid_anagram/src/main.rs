c94940370191563976b84a2fc90381dc5d2579e0
Red Cherry Tree <82998895+38H4CERO@users.noreply.github.com>
2023-09-24 21:12:33 +0200

    Valid Anagram

--- Arrays & Hashing/valid_anagram/src/main.rs
+++ Arrays & Hashing/valid_anagram/src/main.rs
@@ -1,3 +1,22 @@
+fn is_anagram(s: String, t: String) -> bool {
+    if s.len() != t.len() {
+        return false;
+    }
+    let mut s_chars: Vec<char> = s.chars().collect();
+    let mut t_chars: Vec<char> = t.chars().collect();
+    s_chars.sort();
+    t_chars.sort();
+    return s_chars == t_chars;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("anagram");
+    let t1 = String::from("nagaram");
+    let result1 = is_anagram(s1.clone(), t1.clone());
+    println!("Is '{}' an anagram of '{}'? {}", s1, t1, result1);
+
+    let s2 = String::from("rat");
+    let t2 = String::from("car");
+    let result2 = is_anagram(s2.clone(), t2.clone());
+    println!("Is '{}' an anagram of '{}'? {}", s2, t2, result2);
 }
