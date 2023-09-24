c905bbc168fac9fa31d157e1381d0e9c8afd1f5b
Red Cherry Tree <82998895+38H4CERO@users.noreply.github.com>
2023-09-24 20:20:10 +0200

    Hacer Contains Duplicate

--- Arrays & Hashing/contains_dupliclate/src/main.rs
+++ Arrays & Hashing/contains_dupliclate/src/main.rs
@@ -1,3 +1,18 @@
+use std::collections::HashSet;
+
+pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut vistos: HashSet<i32> = HashSet::with_capacity(nums.len()); //Es mas rapido con with_capacity() que new()
+
+    for num in nums.iter() {
+        if !vistos.insert(*num) {
+            return true;
+        }
+    }
+    return false;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![1, 2, 3, 4, 1];
+    let result = contains_duplicate(nums);
+    println!("Contains duplicate: {}", result);
 }
